e19aa741258bb46b7341b6d3f30fdb5a8029af1a
Geonhee Oh <goni@Geonhees-MBP.attlocal.net>
2021-10-13 20:15:04 -0500

    v3

---
+++ v3mass_hist_weight.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+root -l -b <<EOF
+.L v3mass_hist_weight_nominal_211012.C 
+.q
+EOF
+
+echo entering to the loop...
+
+for pt in '3,4.5' '4.5,6.5'
+do
+  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',1.6,2.4,20,120,-1)'
+  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',1.6,2.4,20,120,0)'
+  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',1.6,2.4,20,120,1)'
+done
+
+#for pt in '6.5,9' '9,12' '12,50'
+#do
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',0,2.4,20,120,-1)'
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',0,2.4,20,120,0)'
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',0,2.4,20,120,1)'
+#done
+
+for pt in '3,4.5' '4.5,6.5'
+do
+  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',1.6,2.4,40,80,-1)'
+  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',1.6,2.4,40,80,0)'
+  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',1.6,2.4,40,80,1)'
+done
+
+#for pt in '6.5,9' '9,12' '12,50'
+#do
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',0,2.4,40,80,-1)'
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',0,2.4,40,80,0)'
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C('$pt',0,2.4,40,80,1)'
+#done
+
+#for cent in '0,40' '40,80'
+#do
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C(6.5,50,0,2.4,'$cent',-1)'
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C(6.5,50,0,2.4,'$cent',0)'
+#  root -l -b -q 'v3mass_hist_weight_nominal_211012.C(6.5,50,0,2.4,'$cent',1)'
+#done
